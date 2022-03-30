075aa08a72f21653dfd0e241dd38a9415b104a2f
David Hollinger <david.hollinger@gmail.com>
2022-03-30 12:37:47 -0500

    progress

--- prz.go
+++ prz.go
@@ -1,7 +1,29 @@
 package prz
 
-func Columnf64(table [][]float64, column []float64, colnum int) {
+import (
+	"math"
+)
+
+func GetColumnf64(table [][]float64, column []float64, colnum int) {
 	for i := range table {
 		column[i] = table[i][colnum]
 	}
+}
+
+func SumColumn(column []float64) float64 {
+	var total float64 = 0
+	for i := range column {
+		total += column[i]
+	}
+	return total
+}
+
+func ChiSquared(actual []float64, guess []float64, filter float64) float64 {
+	var fit float64 = 0
+	for i := range actual {
+		if actual[i] > filter {
+			fit += (math.Pow(actual[i]-guess[i], 2) / guess[i])
+		}
+	}
+	return fit
 }
